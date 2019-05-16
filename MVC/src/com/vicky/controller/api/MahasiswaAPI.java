9b41fb042b7b48de602995cd20d8b4913a127149
Taufiq <32932093+metaufiq@users.noreply.github.com>
2019-05-16 17:55:25 +0700

    update mahasiswaAPI

--- MVC/src/com/vicky/controller/api/MahasiswaAPI.java
+++ MVC/src/com/vicky/controller/api/MahasiswaAPI.java
@@ -1,14 +1,21 @@
 package com.vicky.controller.api;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vicky.model.Dosen;
 import com.vicky.model.Pesan;
 import com.vicky.service.DosenInterfaceService;
@@ -22,15 +29,45 @@ public class MahasiswaAPI {
 	private DosenInterfaceService dosenService;
 	
 	
-	@PostMapping("/api/sendMessage")
+	private ObjectMapper objectMapper = new ObjectMapper();
+	
+	@GetMapping("/api/sendMessage")
 	@ResponseBody
-	public String messageSend(Pesan pesan) {
-		pesanService.sendPesan(pesan);
+	public String messageSend(@RequestBody String pesan) {
+		System.out.println(pesan);
+		Pesan pesanObj = new Pesan();
+		
+		try {
+			pesanObj = objectMapper.readValue(pesan, Pesan.class);
+			
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch(NullPointerException e) {
+			e.printStackTrace();
+		}
+		
+		pesanService.sendPesan(pesanObj);
 		return "berhasil";
 	}
 	
 	@GetMapping("/api/getAllDosen")
-	public List<Dosen> getAllDosen() {
-		return dosenService.getAllDosen();
+	@ResponseBody
+	public String getAllDosen() {
+		
+		String jsonStr="cannot transfer data";
+	try {
+			jsonStr = objectMapper.writeValueAsString(dosenService.getAllDosen());
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return jsonStr;
 	}
 }
